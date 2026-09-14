e3f400ae08d3b583604402974b8c4a95cfaa2946
Maciej Małecki <me@mmalecki.com>
2026-09-14 21:58:18 +0200

    Output to stdout

--- into-po-lines-import.sh
+++ into-po-lines-import.sh
@@ -2,5 +2,5 @@
 set -e
 xan select "$CSV_SKU_COLUMN,$CSV_Q_COLUMN,$CSV_ADDITIONAL_COLUMNS" < "$1" > sku-qs.csv
 xan select "$CSV_SKU_COLUMN" < "$1" | (echo "PK" && tail -n+2 | xargs -I'{}' ./sku-to-pk.sh '{}') > sku-pks.csv
-xan cat columns sku-pks.csv sku-qs.csv > po-lines-import.csv
+xan cat columns sku-pks.csv sku-qs.csv
 rm -f sku-pks.csv sku-qs.csv
